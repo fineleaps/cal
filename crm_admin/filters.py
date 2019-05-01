0a6ae0d7b5cec65edfc18edce4ed71521989204c
unknown <Agent14@AGENT14.FLSystems.Local>
2019-05-01 16:54:17 +0530

    crm_admin further views

--- crm_admin/filters.py
+++ crm_admin/filters.py
@@ -10,12 +10,37 @@ from django import forms
 
 
 IND_CHOICES = [(i, i) for i in Prospect.objects.all().values_list('industry_type', flat=True).distinct()]
-IND_CHOICES[IND_CHOICES.index(('', ''))] = ('', 'Blank')
+if ('', '') in IND_CHOICES:
+    IND_CHOICES[IND_CHOICES.index(('', ''))] = ('', 'Blank')
+
+JOB_TITLE_CHOICES = [(i, i) for i in Prospect.objects.all().values_list('job_title', flat=True).distinct()]
+if ('', '') in JOB_TITLE_CHOICES:
+    JOB_TITLE_CHOICES[JOB_TITLE_CHOICES.index(('', ''))] = ('', 'Blank')
+
+STATE_CHOICES = [(i, i) for i in Prospect.objects.all().values_list('state', flat=True).distinct()]
+if ('', '') in STATE_CHOICES:
+    STATE_CHOICES[STATE_CHOICES.index(('', ''))] = ('', 'Blank')
+
+EMP_SIZE_CHOICES = [(i, i) for i in Prospect.objects.all().values_list('emp_size', flat=True).distinct()]
+if ('', '') in EMP_SIZE_CHOICES:
+    EMP_SIZE_CHOICES[EMP_SIZE_CHOICES.index(('', ''))] = ('', 'Blank')
 
 
 class ProspectFilter(django_filters.FilterSet):
+    #
+    # industry_type = django_filters.MultipleChoiceFilter(choices=IND_CHOICES, widget=forms.CheckboxSelectMultiple(
+    #     attrs={'style': "height: 200px; overflow: scroll"}))
+    # job_title = django_filters.MultipleChoiceFilter(choices=JOB_TITLE_CHOICES,  widget=forms.CheckboxSelectMultiple(
+    #     attrs={'style': "height: 200px; overflow: scroll"}))
+    # state = django_filters.MultipleChoiceFilter(choices=STATE_CHOICES,  widget=forms.CheckboxSelectMultiple(
+    #     attrs={'style': "height: 200px; overflow: scroll"}))
+    # emp_size = django_filters.MultipleChoiceFilter(choices=EMP_SIZE_CHOICES,  widget=forms.CheckboxSelectMultiple(
+    #     attrs={'style': "height: 200px; overflow: scroll"}))
 
     industry_type = django_filters.MultipleChoiceFilter(choices=IND_CHOICES, widget=forms.CheckboxSelectMultiple)
+    job_title = django_filters.MultipleChoiceFilter(choices=JOB_TITLE_CHOICES,  widget=forms.CheckboxSelectMultiple)
+    state = django_filters.MultipleChoiceFilter(choices=STATE_CHOICES,  widget=forms.CheckboxSelectMultiple)
+    emp_size = django_filters.MultipleChoiceFilter(choices=EMP_SIZE_CHOICES,  widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Prospect
--- crm_admin/forms.py
+++ crm_admin/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from portal.models import Campaign
+from portal.models import Campaign, Executive
 
 
 class CampaignAddForm(forms.ModelForm):
+
+    # active_status = forms.BooleanField(widget=forms.CheckboxInput(attrs={"class": "form-check"}))
+
+    executives = forms.ModelMultipleChoiceField(queryset=Executive.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Campaign
         fields = ('name', 'active_status', 'start_date', 'end_date',
-                  'script', 'details', 'executives', 'prospects')
+                  'script', 'details', 'executives')
